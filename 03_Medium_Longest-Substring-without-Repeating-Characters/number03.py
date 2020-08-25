ace3ce83b5913db4ebf299d83f5e46ed14fa3b66
Stefan Mendoza <stefan@stefanmendoza.com>
2020-08-24 22:51:18 -0500

    Update No. 3

--- 03_Medium_Longest-Substring-without-Repeating-Characters/number03.py
+++ 03_Medium_Longest-Substring-without-Repeating-Characters/number03.py
@@ -1,14 +1,12 @@
-# Runtime: 168 ms, faster than 22.39% of Python3 online submissions for Longest Substring Without Repeating Characters.
-# Memory Usage: 14 MB, less than 35.57% of Python3 online submissions for Longest Substring Without Repeating Characters.
+# Runtime: 140 ms, faster than 25.37% of Python3 online submissions for Longest Substring Without Repeating Characters.
+# Memory Usage: 14.1 MB, less than 19.28% of Python3 online submissions for Longest Substring Without Repeating Characters.
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         longestLength = 0
-        priorSubstring = []
         substring = []
         
         for current in s:
-            priorSubstring = substring
             substring.append(current)
             
             if substring.count(current) == 2:
